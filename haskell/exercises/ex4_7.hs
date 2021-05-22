c936fe4475eafac4bc4c0d1ae71666c2a519e9da
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-05-22 12:22:40 -0300

    add ex4_7.hs

---
+++ haskell/exercises/ex4_7.hs
@@ -0,0 +1,4 @@
+-- Definir la función aplicarPar/2, que recibe una función y un par, y devuelve el
+-- par que resulta de aplicar la función a los elementos del par.
+
+aplicarPar f (x, y) = (f x, f y)
