b7ffc064e4d05d6d53239494cd1d6826822b3173
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-15 16:54:58 -0300

    add 2_01.pl

---
+++ prolog/exercises/2_01.pl
@@ -0,0 +1,18 @@
+#Una empresa está buscando candidatos para varios de sus sectores. Se sabe que:
+
+#Roque es contador, honesto y joven.
+#Ana es ingeniera y honesta, pero no es joven.
+#Cecilia es abogada.
+#Escribí una base de conocimiento que permita consultarla de la siguiente forma:
+
+#? honesto(ana).
+#yes
+
+contador(roque).
+honesto(roque).
+joven(roque).
+
+ingeniero(ana).
+honesto(ana).
+
+abogado(cecilia).
