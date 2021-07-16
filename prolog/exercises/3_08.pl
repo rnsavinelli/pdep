259bffcd93b49385ccc65f7ee343b71aa1d1db41
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-16 10:50:52 -0300

    add 3_08.pl

---
+++ prolog/exercises/3_08.pl
@@ -0,0 +1,8 @@
+%Parece que el not/1 es fácil de usar, ¿no? Pongamos a prueba lo que acabamos de aprender:
+
+%Escribí un predicado loLleva/2, que relaciona a dos personas cuando la primera puede llevar a la segunda; esto se da cuando son vecinos, la primera tiene auto y la segunda no.
+
+loLleva(PersonaA, PersonaB) :-
+  tieneAuto(PersonaA),
+  not(tieneAuto(PersonaB)),
+  sonVecinos(PersonaA, PersonaB).
