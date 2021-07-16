1403abbf1262f50ab57f9aa2ba68f79d72204222
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-16 10:51:20 -0300

    add 3_15.pl

---
+++ prolog/exercises/3_15.pl
@@ -0,0 +1,16 @@
+%¡Uno más para poner en práctica todo lo que aprendimos en esta lección!
+
+%Hacé un predicado zonaDesierta/1 que se verifique para aquellas zonas en las que no hay habitantes y a su vez se cumple que para todos los destinos que se encuentran allí, nadie quiere ir.
+
+%Tené en cuenta que los predicados viveEn/2, quedaEn/2, quiereIr/2 y zonaHabitada/1 están disponibles para que los uses.
+
+zonaDesierta(Zona) :-
+  not(zonaHabitada(Zona)),
+  forall(quedaEn(Lugar, Zona), nadieQuiereIr(Lugar)).
+
+nadieQuiereIr(Lugar) :-
+  not(alguienQuiereIr(Lugar)).
+
+alguienQuiereIr(Lugar) :-
+  quiereIr(_, Lugar).
+
