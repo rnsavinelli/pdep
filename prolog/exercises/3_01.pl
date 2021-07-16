bf796380a0a0f9a273707c57c278d9e7bc58db33
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-16 10:50:18 -0300

    add 3_01.pl

---
+++ prolog/exercises/3_01.pl
@@ -0,0 +1,31 @@
+%Como vamos a llevar a varias personas en tu auto (ah, ¿no te contamos? ¡Somos varios! Esperamos que tengas lugar ), necesitamos saber dónde viven :
+
+%Mariano y Fede viven en Avellaneda
+%Victoria vive en Versalles
+%Rodrigo vive en Villa Ballester
+%Tomás vive en Núñez (nuniez)
+%Además, necesitamos conocer aquellos lugares que vamos a visitar :
+
+%La utn queda en almagro y en villaLugano (tiene dos sedes)
+%La facultad de exactas y la cancha de river quedan en nuniez
+%La cancha de racing queda en avellaneda
+Con esta información, escribí:
+
+%el predicado viveEn/2, que relaciona a una persona con el lugar donde vive
+%el predicado quedaEn/2, que relaciona a un destino con la zona en la que se encuentra.
+
+viveEn(mariano, avellaneda).
+viveEn(fede, avellaneda).
+
+viveEn(victoria, versalles).
+viveEn(rodrigo, villaBallester).
+
+viveEn(tomas, nuniez).
+
+quedaEn(utn, almagro).
+quedaEn(utn, villaLugano).
+
+quedaEn(exactas, nuniez).
+quedaEn(river, nuniez).
+
+quedaEn(racing, avellaneda).
