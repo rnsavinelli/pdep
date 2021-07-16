3bff6070387e9deec40b66fb3284eb41198565bf
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-16 10:50:58 -0300

    add 3_09.pl

---
+++ prolog/exercises/3_09.pl
@@ -0,0 +1,19 @@
+%Ahora queremos saber si alguien está bien ubicado: esto ocurre cuando vive en una zona en la que están todos los destinos a los que quiere ir. Para eso, contamos con la nueva información de quiereIr/2:
+
+%quiereIr(fede, racing).
+%quiereIr(fede, montoto).
+%quiereIr(victoria, montoto).
+%quiereIr(tomas, montoto).
+%quiereIr(tomas, utn).
+
+%¿Pero cómo podemos hacer para saber si todos los lugares a donde quiere ir están en su zona? ¿Se te ocure alguna forma de resolverlo, usando sólo lo que vimos hasta ahora?
+
+%Escribí el predicado bienUbicado/1. Asumí que ya escribimos quiereIr/2 por vos.
+
+bienUbicado(Persona) :-
+  not(malUbicado(Persona)).
+
+malUbicado(Persona) :-
+  viveEn(Persona, Zona),
+  quiereIr(Persona, Lugar),
+  not(quedaEn(Lugar, Zona)).
