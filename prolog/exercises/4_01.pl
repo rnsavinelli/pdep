8df8347bd71e0ec513808edd0c847ece4630dbfb
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-16 16:39:30 -0300

    add 4_01.pl

---
+++ prolog/exercises/4_01.pl
@@ -0,0 +1,11 @@
+%Tenemos el conocimiento de quiénes trabajan en una empresa, y queremos saber quién trabaja con quién.
+
+%Desarrollá un predicado trabajaCon/2: que relaciona a dos personas distintas cuando ambas trabajan en el mismo sector.
+
+%Asumí que existe un predicado trabajaEn/2, que relacionan a un departamento con un empleado que trabaja allí.
+
+%trabajaCon(Persona1, Persona2)
+trabajaCon(P1, P2) :-
+  trabajaEn(Departamento, P1),
+  trabajaEn(Departamento, P2),
+  P1 \== P2.
