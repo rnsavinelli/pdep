ad4926aae831c308e08aab28910e675242e3b979
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-20 17:50:17 -0300

    add 7_14.pl

---
+++ prolog/exercises/7_14.pl
@@ -0,0 +1,10 @@
+%Escribí un predicado abandono/1 que diga si un equipo abandonó el juego. Esto es verdadero cuando no tiene ningún integrante.
+
+%Asumir que se cuenta con un predicado equipo/1, que dice si un individuo es un equipo, y el predicado perteneceA/2, que relaciona a un integrante con su equipo.
+
+abandono(Equipo) :-
+  equipo(Equipo),
+  forall(
+    (perteneceA(Individuo, _), not(equipo(Individuo))),
+    not(perteneceA(Individuo, Equipo))
+  ).
