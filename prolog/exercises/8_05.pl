6c2d12e4a7c0313b78433b67af9064b9c3ac4f91
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-08-25 20:41:02 -0300

    add 8_05.pl

---
+++ prolog/exercises/8_05.pl
@@ -0,0 +1,23 @@
+%Cuando escribamos patrones de functores, los nombres de los functores no pueden ser variables.
+
+%Por ejemplo, si quiero obtener los nombres de todas las mujeres (sean stark o lannister), lo siguiente NO funciona:
+
+%personajesMujeres(Nombre) :-
+%  personaje(Nombre, _(_, mujer)).
+
+%La forma correcta de hacerlo es la siguiente, asumiendo que solamente hay lannisters y starks en nuestra base de conocimientos:
+
+personajeMujer(Nombre) :-
+  personaje(Nombre, stark(_, mujer)).
+personajeMujer(Nombre) :-
+  personaje(Nombre, lannister(_, mujer)).
+
+%Veamos si queda claro: escribí un predicado personajeAdulto/1, que genera los nombres de personajes mayores a 15, sin importar casa ni sexo.
+
+personajeAdulto(Nombre) :-
+  personaje(Nombre, lannister(Edad, _)),
+  Edad >= 16.
+
+personajeAdulto(Nombre) :-
+  personaje(Nombre, stark(Edad, _)),
+  Edad >= 16.
