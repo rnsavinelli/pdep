a2b727b4b6e37e456857a5eb1b5800b4b61e0087
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-08-25 20:40:41 -0300

    add 8_02.pl

---
+++ prolog/exercises/8_02.pl
@@ -0,0 +1,9 @@
+%Ahora agregá a la base de conocimiento cláusulas necesarias para incorporar la siguiente información:
+
+%arya es una mujer de 14
+%cersei es una mujer de 34
+
+%Agregá las cláusulas necesarias al predicado personaje/2
+
+personaje(arya, (14, mujer)).
+personaje(cersei, (34, mujer)).
